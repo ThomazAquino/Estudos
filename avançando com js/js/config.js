d6201a6647cac82b4fa704fd627a93199996c9db
Thomaz Aquino <thomaz.aquino@tekoa.com.br>
2018-03-26 01:40:20 -0300

    aula 3 avançando com php

---
+++ avançando com js/js/config.js
@@ -0,0 +1,9 @@
+function setConfig() {
+var texts = {
+    "title":"Shopping Controol"
+};
+document.title = texts.title;
+document.getElementById("navTitle").innerHTML = texts.title;
+}
+
+setConfig();
